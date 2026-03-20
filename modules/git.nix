85420089879826568ad11d59357d4842abd22cc1
faq0n <faq0n@mailbox.org>
2026-03-20 17:42:01 +0100

    renew: token

--- modules/git.nix
+++ modules/git.nix
@@ -6,7 +6,7 @@
         #"https://oauth2:${secrets.github.oauth_token}@github.com" = {
         #  insteadOf = "https://github.com";
         #};
-        "https://oauth2:${secrets.gitlab.oauth_token}@gitlab.com" = {
+        "https://fq1rex:${secrets.gitlab.access_token}@gitlab.com" = {
           insteadOf = "https://gitlab.com";
         };
       };
