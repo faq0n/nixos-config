8ebfd183367337e5c88fe3d6f0d513afbea21789
faq0n <faq0n@mailbox.org>
2026-03-30 17:53:42 +0200

    update: flake.nix

--- flake.nix
+++ flake.nix
@@ -95,6 +95,7 @@
 	    # TODO replace user with your own username
             home-manager.users.user = import ./home.nix;
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+	    home-manager.extraSpecialArgs = { inherit inputs secrets; };
 	  }
         ];
       };
