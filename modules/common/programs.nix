4b11ad6a4543ee5c3f0a6ecaafabed1274b370e1
faQun <148352661+faq0n@users.noreply.github.com>
2026-04-01 14:35:56 +0200

    Add direnv to bash

--- modules/common/programs.nix
+++ modules/common/programs.nix
@@ -9,4 +9,53 @@
      pwgen
   ];
 
+  programs = {
+    bash = {
+      enable = true;
+      promptInit = ''
+        # Simple readable prompt (Crostini has poor defaults)
+        export PS1="\[\e[1;32m\]\u@\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ "
+
+        # Better defaults
+        export EDITOR=nvim
+        export PAGER=less
+
+        # History improvements
+        export HISTSIZE=10000
+        export HISTFILESIZE=20000
+        shopt -s histappend
+        shopt -s checkwinsize
+
+        # Direnv hook
+        eval "$(direnv hook bash)"
+
+        # fzf keybindings
+        #[ -f ${pkgs.fzf}/share/fzf/key-bindings.bash ] && source ${pkgs.fzf}/share/fzf/key-bindings.bash
+        #[ -f ${pkgs.fzf}/share/fzf/completion.bash ] && source ${pkgs.fzf}/share/fzf/completion.bash
+       '';
+
+      shellAliases = {
+        grep = "rg";
+
+        cd = "z";
+        ".." = "cd ..";
+        "..." = "cd ../..";
+      };
+    };
+
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+
+    git = {
+      enable = true;
+      config = {
+        init.defaultBranch = "main";
+        pull.rebase = false;
+      };
+    };
+  };
+
+
 }
