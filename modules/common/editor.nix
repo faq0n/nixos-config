88e54509a5026916ab4ab6cc06c391bcd37291da
faq0n <faqun@mailbox.org>
2026-04-07 15:11:57 +0200

    fix: nvim pkgs

--- modules/common/editor.nix
+++ modules/common/editor.nix
@@ -2,22 +2,4 @@
 
    programs.neovim.enable = true;
    programs.neovim.vimAlias = true;
-   progams.neovim.defaultEditor = true;
-   programs.neovim.configure = {
-     customRC = ''
-    " here your custom VimScript configuration goes!
-  '';
-  customLuaRC = ''
-    -- here your custom Lua configuration goes!
-  '';
-  packages.myVimPackage = with pkgs.vimPlugins; {
-    # loaded on launch
-    start = [ fugitive 
-	obsidian-nvim
-    ];
-    # manually loadable by calling `:packadd $plugin-name`
-    opt = [ opencode-nvim ];
-  };
-  environment.systemPackages =  with pkgs; [ fzf ripgrep ];
-
 }
