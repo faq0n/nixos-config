16f2f8063806e41b7ef53b3aec4e26dd81a086d6
faQun <faqun@mailbox.org>
2026-05-27 10:08:49 +0200

    Update disko-config.nix

--- hosts/ursus/disko-config.nix
+++ hosts/ursus/disko-config.nix
@@ -7,96 +7,69 @@
 #   disko.devices.disk.data2.device = "/dev/sdc";
 # }
 {
-  disko.devices = {
-    disk = {
-      root = {
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                # LUKS passphrase will be prompted interactively only
-                type = "luks";
-                name = "crypted";
-                settings = {
-                  allowDiscards = true;
-                };
-                content = {
-                  type = "filesystem";
-                  format = "ext4";
-                  mountpoint = "/";
-                };
-              };
-            };
-          };
-        };
-      };
-      data1 = {
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "data";
-              };
-            };
-          };
-        };
-      };
-      data2 = {
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            zfs = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "data";
-              };
-            };
-          };
-        };
+  fileSystems."/nix".neededForBoot = true;
+  fileSystems."/persist".neededForBoot = true; # sometimes needed too
+
+  disko.devices.nodev = {
+    "/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "size=25%"
+        "mode=755"
+      ];
+    };
+  };
+
+  disko.devices.disk.main = {
+    device = "/dev/vda";
+    type = "disk";
+
+    content.type = "gpt";
+
+    content.partitions.boot = {
+      name = "boot";
+      size = "1M";
+      type = "EF02";
+    };
+
+    content.partitions.esp = {
+      name = "ESP";
+      size = "1G";
+      type = "EF00";
+
+      content = {
+        type = "filesystem";
+        format = "vfat";
+        mountpoint = "/boot";
       };
     };
-    zpool = {
-      data = {
-        type = "zpool";
-        mode = "mirror";
-        rootFsOptions = {
-          compression = "zstd";
-          "com.sun:auto-snapshot" = "true";
-        };
-        postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^data@blank$' || zfs snapshot data@blank";
 
-        datasets = {
-          "encrypted" = {
-            type = "zfs_fs";
-            mountpoint = "/data";
-            options = {
-              encryption = "aes-256-gcm";
-              keyformat = "passphrase";
-              keylocation = "file:///tmp/secret.key";
-            };
-            # use this to read the key during boot
-            postCreateHook = ''
-              zfs set keylocation="prompt" "data/encrypted";
-            '';
+    content.partitions.swap = {
+      size = "4G";
+
+      content = {
+        type = "swap";
+        resumeDevice = true;
+      };
+    };
+
+    content.partitions.root = {
+      name = "root";
+      size = "100%";
+
+      content = {
+        type = "btrfs";
+        extraArgs = ["-f"];
+
+        subvolumes = {
+          "/persist" = {
+            mountOptions = ["subvol=persistent" "noatime"];
+            mountpoint = "/persistent";
+          };
+
+          "/nix" = {
+            mountOptions = ["subvol=nix" "noatime"];
+            mountpoint = "/nix";
           };
         };
       };
